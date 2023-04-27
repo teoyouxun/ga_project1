e0447e581b6b53e09d5be82fd361153a56d23f54
YX <asdf@hotmail.com>
2023-04-27 10:08:42 +0800

    added css gradient bg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_back_or_to @post
     else
       render :new
     # redirect_to root_path
